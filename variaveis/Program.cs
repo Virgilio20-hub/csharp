d0b873cec01b45de6f2d9df742c7e97974900e29
gabgrabit <gabrielhenriquef11@outlook.com>
2024-11-01 16:07:54 -0400

    Tipos de dados Primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Tipos de dados Primitivos
+// Numeros inteiros
+short ano = 2025;
+int contador = 10;
+long populacaoMundial = 7800000000;
+
+// Numeros de ponto flutuante
+float temperatura = 43.6f;
+double precoProduto = 199.90;
+
+// Tipos Caracter
+char inicial = "W";
+
+// Tipos Logicos
+bool estaLogado = true;
+bool ativo = false;
