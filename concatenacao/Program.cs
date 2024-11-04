15570c08a3f99b58b08fa8403275aef68e292d94
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-04 17:02:20 -0400

    Interpolacao de strings

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -6,3 +6,9 @@ string nome = "gabriel";
 string saudacao = "ola, " + nome + "! bem-vindo.";
 Console.WriteLine(saudacao);
 
+// Interpolacao de strings
+string nome1 = "gabriel";
+string saudacao1 = $"ola, {nome1}! bem-vindo.";
+Console.WriteLine(saudacao1);
+
+
