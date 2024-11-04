8911d0d80fdf20124b075e3e9fce387cfe853886
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-04 17:24:13 -0400

    Metodo Parse

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -11,4 +11,17 @@ string nome1 = "gabriel";
 string saudacao1 = $"ola, {nome1}! bem-vindo.";
 Console.WriteLine(saudacao1);
 
+// Operador GetType()
+int numero = 5;
+Console.WriteLine(numero.GetType());
 
+// Palavra-chave typeof
+Console.WriteLine(typeof(int));
+
+// Metodo Parse
+string valorTexto = "100";
+Console.WriteLine("A variavel valorTexto e do tipo:" + valorTexto.GetType());
+
+int numero1 = int.Parse(valorTexto);
+Console.WriteLine(numero1);
+Console.WriteLine("A variavel numeroParse e do tipo:" +numero1.GetType());
