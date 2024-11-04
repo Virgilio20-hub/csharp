a1befd10edc7520b5d204497696a93a2a8cc979f
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-04 16:55:12 -0400

    ADD projeto concatenacao

---
+++ concatenacao/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Concatenacao
+string nome = "gabriel";
+string saudacao = "ola, " + nome + "! bem-vindo.";
+Console.WriteLine(saudacao);
+
