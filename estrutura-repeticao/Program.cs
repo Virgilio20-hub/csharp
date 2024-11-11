4da6fce9d4d18380ecb6cdc50c2c2a601cfa018c
gabgrabit <gabrielhenriquef11@outlook.com>
2024-11-11 15:03:53 -0400

    Estrutura de repeticao for

--- estrutura-repeticao/Program.cs
+++ estrutura-repeticao/Program.cs
@@ -1,22 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Estrutura de Repeticao while
-Random numeroAleatorio = new Random();
-int numero = numeroAleatorio.Next(1, 101);
-int contador = 0;
-Console.WriteLine($"Numero Alvo: {numero}");
-// Loop que incrementa o contador ate ele atingir o numero alvo
-while (contador != numero)
-{
-    Console.WriteLine($"Contador: {contador}.");
-    contador++;
-}
-Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
+// Console.WriteLine("Hello, World!");
+// // Estrutura de Repeticao while
+// Random numeroAleatorio = new Random();
+// int numero = numeroAleatorio.Next(1, 101);
+// int contador = 0;
+// Console.WriteLine($"Numero Alvo: {numero}");
+// // Loop que incrementa o contador ate ele atingir o numero alvo
+// while (contador != numero)
+// {
+//     Console.WriteLine($"Contador: {contador}.");
+//     contador++;
+// }
+// Console.WriteLine($"Contador atingiu o numero alvo: {contador}");
 //Estrutura de Repeticao do-while
-int numero = 11;
+// int numero = 0;
 
-do
+// do
+// {
+//     Console.WriteLine($"numero: {numero}");
+//     numero++;
+// } while (numero < 10);
+// Estrutura de repeticao for
+for (int i = 0; i <= 10; i++)
 {
-    Console.WriteLine($"numero: {numero}");
-    numero++;
-} while (numero < 10);
+    Console.WriteLine($"valor de i: {i}");
+}
