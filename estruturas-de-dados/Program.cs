e1afd98c580840ef000d182874770a2a1c1a2358
gabgrabit <gabrielhenriquef11@outlook.com>
2024-11-18 16:47:51 -0400

    elementos repetidos no conjunto

---
+++ estruturas-de-dados/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// elementos repetidos no conjunto
+// HashSet<int> conjunto = new HashSet<int> { 1, 2, 3 };
+// conjunto.Add(4);
+// conjunto.Add(2);
+
+// Console.WriteLine("Elementos do Conjunto:");
+// foreach (int elemento in conjunto)
+// {
+//     Console.WriteLine(elemento);
+// }
+
+// ashSet<string> frutas = new HashSet<string> { "Maca", "Abacaxi", "Laranja" };
+// frutas.Add("Uva");
+// frutas.Add("Maca");
+
+// Console.WriteLine("Frutas no Conjunto:");
+// foreach (string fruta in frutas)
+// {
+//     Console.WriteLine(fruta);
+// }H
+
+// Console.WriteLine("\nVerificacao de existencia:");
+// Console.WriteLine(frutas.Contains("Banana") ? "Banana esta no conjunto" : "Banana nao esta no conjunto");
+
+// // add remove
+// frutas.Remove("Laranja");
+// Console.WriteLine("\nApos remover Laranja:");
+// foreach (string fruta in frutas)
+// {
+//     Console.WriteLine(fruta);
+// }
+
+// exercicios
+// 1. dado um array de inteiros int[] numeros = { 1, 2, 3, 4, 5 };, escreva um codigo que some todos os elementos do array e exiba o resultado no console.
+// 2. dada uma lista de strings List<string> nomes = new List<string> {"Jeane", "Wanderson", "Jeane", "Ryan", "Jeane" };, escreva um codigo para contar quantas vezes o nome "Jeane" aparece na lista.
+// 3. dada uma lista de inteiros com elementos duplicados List<int> numeros = new List<int> {1, 2, 2, 3, 4, 4, 5 };, escreva um codigo para remover os elementos duplicados e exibir a lista sem duplicatas.
+// 4. dado um conjunto HashSet<string> frutas = new HashSet<string> {"Maca", "Banana", "Laranja" };, escreva um codigo que verifique se "Banana" esta no conjunto e exiba uma mensagem no console.
+// 5. Crie uma lista vazia de numeros inteiros List<int> numeros = new List<int>();. em seguida, adicione os numeros de 1 a 5 na lista e exiba os elementos no console.
+
