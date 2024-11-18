222302e6bdfa7c3c9ad3a8ea6c4f9cfc1cd11bef
gabgrabit <gabrielhenriquef11@outlook.com>
2024-11-18 16:57:05 -0400

    exercicios

---
+++ exercicios/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*int idade = 18;
+string mensagem = idade >= 18 ? "Maior de idade";
+Console.WriteLine("Entrada Liberada");
+
+int idade = 18;
+string mensagem = idade <= 18 ? "Menor de idade";
+Console.WriteLine("Entrada nao permitida");
+*/
+/*int idade1 >= 18;
+string entrada1 = $"Qual é a sua idade? {idade1}. Entrada permitida";
+Console.WriteLine(entrada1);
+
+int idade2 <= 17;
+string entrada2 = $"Qual é a sua idade? {idade2}. Entrada nao permitida";
+Console.WriteLine(entrada2);
+*/
+// Exercícios
+/*
+bool ehMaiorIdade = false;
+bool possuiAutorizacaoResponsavel = true;
+
+if(ehMaiorIdade || possuiAutorizacaoResponsavel);
+{
+    Console.WriteLine("Entreda liberada");
+}
+else
+{
+    Console.WriteLine("Entreda liberada");
+}
+*/
+/*bool ehMaiorIdade = false;
+bool possuiAutorizacaoResponsavel = false;
+
+string resultado = ehMaiorIdade || possuiAutorizacaoResponsavel ? "Entrada liebrada" : "Entrada nao liberada";
+
+Console.WriteLine(resultado);
+
+bool possuiPresençaMinima = true;
+double media = 7.0;
+
+if (possuiPresençaMinima && media >= 7)
+{
+    Console.WriteLine($"Média {media:F1}: Aprovado");
+}
+else
+{
+Console.WriteLine($"Média {media:F1}: Reprovado");
+}
+*/
+// bool choveu = true;
+// bool estaTarde = false;
+
+// if(!choveu && !estaTarde)
+// {
+//     Console.WriteLine("vou pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("vou pedalar outro dia");
+// }
